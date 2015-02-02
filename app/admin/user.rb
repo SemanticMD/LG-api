144ff17e4d211fe32da78a25db2593768e763d7e
Isaac Ezer <isaac.ezer@gmail.com>
2015-02-02 14:55:38 -0500

    add organization to user admin page

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -5,6 +5,7 @@ ActiveAdmin.register User do
     selectable_column
     id_column
     column :email
+    column :organization
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -12,6 +13,7 @@ ActiveAdmin.register User do
   end
 
   filter :email
+  filter :organization
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
@@ -19,6 +21,7 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs "Admin Details" do
       f.input :email
+      f.input :organization
       f.input :password
       f.input :password_confirmation
     end
