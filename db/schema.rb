97820f3990ea06496a0b6768178ebb2dbd2b6025
Cory Forsyth <cory.forsyth@gmail.com>
2015-02-06 15:18:05 -0500

    Add missing trackable columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205225145) do
+ActiveRecord::Schema.define(version: 20150206195604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,8 @@ ActiveRecord::Schema.define(version: 20150205225145) do
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "authentication_token"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
