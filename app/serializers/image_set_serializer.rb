9bb59709c44a3f7a633abe012d8ca7541bb4de66
Isaac Ezer <isaac.ezer@gmail.com>
2015-02-12 19:14:17 -0500

    prevent circular embedding

--- app/serializers/image_set_serializer.rb
+++ app/serializers/image_set_serializer.rb
@@ -8,10 +8,18 @@ class ImageSetSerializer < BaseSerializer
     property :user_id, item.uploading_user_id
     property :main_image_id, item.main_image.id if item.main_image
     property :has_cv_results, !item.cv_results.empty?
-    property :lion_id, item.lion.id if item.lion
     property :cv_request_id, item.cv_request.id if item.cv_request
 
     entities :images, item.images, ImageSerializer
     entity :uploading_organization, item.uploading_organization, OrganizationSerializer
+
+    # Avoid infinite circular embeds
+    if context[:embedded]
+      property :lion_id, item.lion.id if item.lion
+    else
+      if item.lion
+        entity :lion, item.lion, LionSerializer, embedded: true
+      end
+    end
   end
 end
--- app/serializers/lion_serializer.rb
+++ app/serializers/lion_serializer.rb
@@ -4,7 +4,13 @@ class LionSerializer < BaseSerializer
 
     map_properties :id, :name, :primary_image_set_id, :gender, :age
 
-    entities :image_sets, item.image_sets, ImageSetSerializer
+    # Avoid infinite circular embeds
+    if context[:embedded]
+      property :image_set_ids, item.image_sets.map(&:id)
+    else
+      entities :image_sets, item.image_sets, ImageSetSerializer, embedded: true
+    end
+
     entity   :organization, item.organization, OrganizationSerializer if item.organization
   end
 end
