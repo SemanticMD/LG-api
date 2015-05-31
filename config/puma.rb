0ddb3022809d20cac56ea56ca4f3008c7d03abb2
Cory Forsyth <cory.forsyth@gmail.com>
2015-05-31 13:41:02 -0400

    remove sidekiq shutdown code

--- config/puma.rb
+++ config/puma.rb
@@ -12,10 +12,4 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
-
-  # https://github.com/mperham/sidekiq/issues/627
-  Sidekiq.redis.shutdown { |conn|
-    Rails.logger.info "Sidekiq shutting redis connection #{conn}"
-    conn.close
-  }
 end
