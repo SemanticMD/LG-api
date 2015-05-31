b699e573cb1ad9ff56ceaf8d3d55fcf5ee65b9e4
Cory Forsyth <cory.forsyth@gmail.com>
2015-05-31 12:32:03 -0400

    Don't throw ActiveRecord::RecordNotFound when looking up cv request

--- app/workers/cv_results_worker.rb
+++ app/workers/cv_results_worker.rb
@@ -9,7 +9,7 @@ class CvResultsWorker
   end
 
   def perform(cv_request_id)
-    @cv_request = CvRequest.find(cv_request_id)
+    @cv_request = CvRequest.find_by_id(cv_request_id)
     if !@cv_request
       Rails.logger.error "CvResultsWorker: no Cv Request found with id #{cv_request_id}"
     elsif !@cv_request.server_uuid
