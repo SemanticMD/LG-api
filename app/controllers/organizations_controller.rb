48ee38fb562b6ffedff4e2637983283d9bd2bb92
Cory Forsyth <cory.forsyth@gmail.com>
2015-06-07 12:17:19 -0400

    Add Organizations#show route

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,4 +2,11 @@ class OrganizationsController < ApiController
   def index
     render json: OrganizationsSerializer.new(Organization.all)
   end
+
+  def show
+    @organization = Organization.find_by_id params[:id]
+    return error_not_found('organization not found') unless @organization
+
+    render json: OrganizationSerializer.new(@organization)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   resources :image_sets, only:[:create, :show, :update, :destroy, :index], path: 'imageSets'
   resources :images, only:[:create, :show, :update, :destroy]
   resources :lions, only:[:show, :index, :create, :update, :destroy]
-  resources :organizations, only: [:index]
+  resources :organizations, only: [:index, :show]
   resources :search_options, only: [:index]
 
   resources :uploads, only:[:create]
